d2bd66262559fc0d125c6fa3d741acaa84e32b4f
fakhar.ahmad <[MNOSOLTNS-10310]>
2023-06-09 15:58:18 -0400

    Added back button in StarMessageActivity.java

--- app/src/main/java/com/samsung/whatsapp/view/activities/StarMessageActivity.java
+++ app/src/main/java/com/samsung/whatsapp/view/activities/StarMessageActivity.java
@@ -1,5 +1,6 @@
 package com.samsung.whatsapp.view.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -7,14 +8,11 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import com.samsung.whatsapp.R;
-import com.samsung.whatsapp.adapters.MessagesAdapter;
 import com.samsung.whatsapp.adapters.StarredMessagesAdapter;
-import com.samsung.whatsapp.adapters.UserAdapter;
 import com.samsung.whatsapp.databinding.ActivityStarMessageBinding;
 import com.samsung.whatsapp.utils.Utils;
-import com.samsung.whatsapp.viewmodel.ContactsViewModel;
 import com.samsung.whatsapp.viewmodel.StarredMessageViewModel;
 
 import java.util.Objects;
@@ -29,8 +27,7 @@ public class StarMessageActivity extends AppCompatActivity {
         binding = ActivityStarMessageBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.mainPageToolbar.mainAppBar);
-        Objects.requireNonNull(getSupportActionBar()).setTitle("Starred Messages");
+        initToolBar();
 
         binding.starMessagesList.setLayoutManager(new LinearLayoutManager(this));
 
@@ -41,5 +38,21 @@ public class StarMessageActivity extends AppCompatActivity {
         adapter = new StarredMessagesAdapter(this, viewModel.getStarredMessage().getValue());
         binding.starMessagesList.addItemDecoration(new DividerItemDecoration(binding.starMessagesList.getContext(), DividerItemDecoration.VERTICAL));
         binding.starMessagesList.setAdapter(adapter);
+    }
+
+    private void initToolBar() {
+        setSupportActionBar(binding.mainPageToolbar.mainAppBar);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Starred Messages");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
